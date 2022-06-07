f644fc34dd81f6b2c02b3b06a93fd8b342e2712c
Donald Brooks Arum <brooksd079@gmail.com>
2022-06-07 21:35:56 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = (parameter1, parameter2) => 2000 / 100;
+
+//Square arrow function
+const square = (x) => x * x;
+
+//square arrow function
+const add = (param1, param2) => param1 + param2;
